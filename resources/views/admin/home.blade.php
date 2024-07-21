9aa1c76f5640a51f23490cc3a77840dcd6a1eef5
Fajar Sidik Prasetio <fajarsidik1999@gmail.com>
2024-07-21 23:26:13 +0700

    initial commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+    <div>Home</div>
+@endsection
---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Registrasi Notaris</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+</head>
+
+<body class="antialiased">
+    @include('layout.sidebar')
+    <div class="p-4 sm:ml-64">
+        @yield('content')
+    </div>
+</body>
+
+</html>
