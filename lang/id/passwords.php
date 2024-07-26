5661953b305b9ad79e86ee4466e96159ecdc2f21
Fajar Sidik Prasetio <fajarsidik1999@gmail.com>
2024-07-26 09:26:10 +0700

    feat: add locale

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Kata sandi berhasil diubah.',
+    'sent' => 'Kami telah mengirim email untuk mengatur ulang kata sandi.',
+    'throttled' => 'Tunggu sebelum mencoba lagi.',
+    'token' => 'Token untuk reset kata sandi tidak valid. Cobalah meminta reset kata sandi ulang.',
+    'user' => "Email tidak ditemukan.",
+
+];
